85d1d2561218dceee8920252ef8b84fc28d37d6f
Hwan Seob Lee <bestseob93@gmail.com>
2017-11-22 13:51:26 +0900

    footer info edited

--- src/components/Base/Footer.js
+++ src/components/Base/Footer.js
@@ -10,7 +10,6 @@ function Footer(props) {
                                 <ul className="style">
                                     <li><a className="ns-L" target="_blank" rel="noopener noreferrer" href="http://intro.funfur.kr/">회사소개</a></li>
                                     <li><Link to="/Policy"><span className="ns-L">이용약관</span></Link></li>
-                                    <li><span className="ns-L">광고운영정책</span></li>
                                     <li><span className="ns-L">개인정보 처리방침</span></li>
                                 </ul>
                             </div>
@@ -21,14 +20,14 @@ function Footer(props) {
                             <div className="col-lg-4 col-lg-offset-3">
                                 <p>
                                     {/*<!-- <img res="img/funfur-icon.png"> -->*/}
-                                    <strong><span className="funfur-color">뻔뻐, Inc.</span></strong><br/> 대표: 유덕열 외 3명<br/> 주소: 서울시 송파구 방이동 62-8 석정빌딩 502호 <br/> 전화: 050-6591-6200 팩스: 0504-433-6202 <br/> 제휴문의: funfurofficial@gmail.com
+                                    <strong><span className="funfur-color">상호명: 뻔뻔한친구들</span></strong><br/> 대표: 유덕열 외 2명<br/> 주소: 서울특별시 광진구 군자로6길 22, 2층 202호 (화양동) <br/> 전화: 050-6591-6200 팩스: 0504-433-6202 <br/> 제휴문의: funfurofficial@gmail.com
                                 </p>
                             </div>
 
                             <div className="col-lg-3">
                                 <p className="text-color">
 
-                                    <br/><br/> 사업자 번호: 435-15-00646<br/> 통신판매업 신고번호: 서울 송파-1234호<br/> 문의 가능 시간: 평일 09:00 ~ 18:00
+                                    <br/><br/> 사업자 번호: 435-15-00646<br/> 통신판매업 신고번호: 2017-서울광진-0996<br/> 문의 가능 시간: 평일 09:00 ~ 18:00
                                 </p>
                             </div>
                         </div>
