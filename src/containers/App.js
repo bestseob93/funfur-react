10acab6a68f3ba131ab04620798844dab24b0afe
Hwan Seob Lee <bestseob93@gmail.com>
2017-11-23 10:06:48 +0900

    a

--- src/containers/App.js
+++ src/containers/App.js
@@ -46,22 +46,20 @@ class App extends Component {
     const { AuthActions } = this.props;
     
     storage.get('token').then(async (value) => {
-      if(value) {
-        try {
-          await AuthActions.checkToken(value);
-          
-        } catch (e) {
-          //document.location="/";
-          // pathname이 /ceo 로 시작하는지 검사.
-          const pathNameRegx = /^\/ceo/g;
+      try {
+        await AuthActions.checkToken(value);
+        
+      } catch (e) {
+        //document.location="/";
+        // pathname이 /ceo 로 시작하는지 검사.
+        const pathNameRegx = /^\/ceo/g;
 
-          // // 로그인 안되어 있는데, ceo 페이지 진입 시 홈으로 강제 이동
-          if(!this.props.authenticated && window.location.pathname.search(pathNameRegx) === 0) {
-            document.location = "/";
-          }
-          
-          if(e) throw e;
+        // // 로그인 안되어 있는데, ceo 페이지 진입 시 홈으로 강제 이동
+        if(!this.props.authenticated && window.location.pathname.search(pathNameRegx) === 0) {
+          document.location = "/";
         }
+        
+        if(e) throw e;
       }
     }).catch(err => {
       if(err) throw err;
